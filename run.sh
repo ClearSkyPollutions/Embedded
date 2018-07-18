c2acd6957b409c9ae25c2f921077420448c20def
nrutemby <nilgabiro@yahoo.fr>
2018-07-18 13:33:49 +0200

    use $! to get the pid of sensor_control

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 cp ./Installation/onRPi/html/* /var/www/html/
 touch log.txt
 python3 ./Sensors/RaspberryPi/sensor_control.py &
-PID=$(pidof python3 ./Sensors/RaspberryPi/sensor_control.py)
+#PID=$(pidof python3 ./Sensors/RaspberryPi/sensor_control.py)
+PID=$!
 echo $PID
 while true; do    
     RECEIVED=$(cat /var/www/html/clearSky.fifo)
